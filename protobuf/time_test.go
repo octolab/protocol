ef46afb51244791be947dd8a444efd7fcc26e0f9
Kamil Samigullin <kamil@samigullin.info>
2020-01-24 22:28:27 +0300

    update deps, review tests

--- protobuf/time_test.go
+++ protobuf/time_test.go
@@ -12,42 +12,68 @@ import (
 )
 
 func TestTime(t *testing.T) {
-	tests := []struct {
-		name      string
+	tests := map[string]struct {
 		timestamp *timestamp.Timestamp
 		assert    func(assert.TestingT, *timestamp.Timestamp)
 	}{
-		{"nil pointer", nil, func(t assert.TestingT, tsp *timestamp.Timestamp) { assert.Nil(t, Time(tsp)) }},
-		{"normal use", new(timestamp.Timestamp), func(t assert.TestingT, tsp *timestamp.Timestamp) {
-			assert.NotNil(t, Time(tsp))
-		}},
-		{"invalid timestamp", func() *timestamp.Timestamp {
-			tsp := timestamp.Timestamp{Seconds: -1, Nanos: -1}
-			return &tsp
-		}(), func(t assert.TestingT, ts *timestamp.Timestamp) { assert.Panics(t, func() { Time(ts) }) }},
+		"nil pointer": {
+			nil,
+			func(t assert.TestingT, tsp *timestamp.Timestamp) {
+				assert.Nil(t, Time(tsp))
+			},
+		},
+		"normal use": {
+			new(timestamp.Timestamp),
+			func(t assert.TestingT, tsp *timestamp.Timestamp) {
+				assert.NotNil(t, Time(tsp))
+			},
+		},
+		"invalid timestamp": {
+			func() *timestamp.Timestamp {
+				tsp := timestamp.Timestamp{Seconds: -1, Nanos: -1}
+				return &tsp
+			}(),
+			func(t assert.TestingT, ts *timestamp.Timestamp) {
+				assert.Panics(t, func() { Time(ts) })
+			},
+		},
 	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
 			test.assert(t, test.timestamp)
 		})
 	}
 }
 
 func TestTimestamp(t *testing.T) {
-	tests := []struct {
-		name   string
+	tests := map[string]struct {
 		time   *time.Time
 		assert func(assert.TestingT, *time.Time)
 	}{
-		{"nil pointer", nil, func(t assert.TestingT, tp *time.Time) { assert.Nil(t, Timestamp(tp)) }},
-		{"normal use", new(time.Time), func(t assert.TestingT, tp *time.Time) { assert.NotNil(t, Timestamp(tp)) }},
-		{"invalid time", func() *time.Time {
-			tp := time.Now().AddDate(-math.MaxInt32, -math.MaxInt32, -math.MaxInt32)
-			return &tp
-		}(), func(t assert.TestingT, tp *time.Time) { assert.Panics(t, func() { Timestamp(tp) }) }},
+		"nil pointer": {
+			nil,
+			func(t assert.TestingT, tp *time.Time) {
+				assert.Nil(t, Timestamp(tp))
+			},
+		},
+		"normal use": {
+			new(time.Time),
+			func(t assert.TestingT, tp *time.Time) {
+				assert.NotNil(t, Timestamp(tp))
+			},
+		},
+		"invalid time": {
+			func() *time.Time {
+				tp := time.Now().AddDate(-math.MaxInt32, -math.MaxInt32, -math.MaxInt32)
+				return &tp
+			}(),
+			func(t assert.TestingT, tp *time.Time) {
+				assert.Panics(t, func() { Timestamp(tp) })
+			},
+		},
 	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
 			test.assert(t, test.time)
 		})
 	}
