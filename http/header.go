39f90320c5946ef2936f14b3ec461d2a72be1634
Kamil Samigullin <kamil@samigullin.info>
2020-03-09 20:22:04 +0300

    remove deprecated methods from header

--- http/header.go
+++ http/header.go
@@ -4,35 +4,16 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-	"time"
 )
 
 const (
 	CacheControlHeader = "Cache-Control"
 	ContentTypeHeader  = "Content-Type"
-	XDeadlineHeader    = "X-Deadline"
 	XStrictHeader      = "X-Strict"
-	XTimeoutHeader     = "X-Timeout"
 )
 
 // Header extends built-in http.Header.
 type Header http.Header
-
-// Deadline returns the deadline duration from the header or the present duration.
-// It also tries to use the timeout duration from the header if the deadline is invalid.
-//
-// Deprecated: use go.octolab.org/toolkit/protocol/http/header.Deadline instead.
-// TODO:v0.0.10 will be removed
-func (header Header) Deadline(fallback time.Duration) (time.Time, bool) {
-	deadline, err := time.Parse(time.RFC3339Nano, http.Header(header).Get(XDeadlineHeader))
-	if err == nil {
-		return deadline, true
-	}
-	if duration, present := header.Timeout(fallback); present {
-		return time.Now().Add(duration), true
-	}
-	return time.Now().Add(fallback), false
-}
 
 // NoCache returns true if the header has no-cache duration of cache control.
 func (header Header) NoCache() bool {
@@ -46,16 +27,4 @@ func (header Header) Strict() bool {
 		strict, _ = strconv.ParseBool(v)
 	}
 	return strict
-}
-
-// Timeout returns the timeout duration from the header or the present duration.
-//
-// Deprecated: use go.octolab.org/toolkit/protocol/http/header.Timeout instead.
-// TODO:v0.0.10 will be removed
-func (header Header) Timeout(fallback time.Duration) (time.Duration, bool) {
-	duration, err := time.ParseDuration(http.Header(header).Get(XTimeoutHeader))
-	if err != nil {
-		return fallback, false
-	}
-	return duration, true
 }
--- http/header_test.go
+++ http/header_test.go
@@ -2,66 +2,11 @@ package http_test
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	. "go.octolab.org/toolkit/protocol/http"
 )
-
-func TestHeader_Deadline(t *testing.T) {
-	type tuple struct {
-		deadline time.Time
-		present  bool
-	}
-
-	now := time.Now()
-
-	tests := map[string]struct {
-		header    Header
-		fallback  time.Duration
-		expected  tuple
-		precision time.Duration
-	}{
-		"exists in header": {
-			Header{XDeadlineHeader: []string{now.Format(time.RFC3339Nano)}},
-			time.Second,
-			tuple{now, true},
-			0,
-		},
-		"exists in header, inaccuracy": {
-			Header{XDeadlineHeader: []string{now.Format(time.RFC3339)}},
-			time.Second,
-			tuple{now, true},
-			time.Second,
-		},
-		"exists in header, timeout": {
-			Header{XTimeoutHeader: []string{"100ms"}},
-			time.Second,
-			tuple{now.Add(100 * time.Millisecond), true},
-			10 * time.Millisecond,
-		},
-		"fallback cause empty": {
-			nil,
-			time.Second,
-			tuple{now.Add(time.Second), false},
-			10 * time.Millisecond,
-		},
-		"fallback cause invalid": {
-			Header{XDeadlineHeader: []string{"invalid"}, XTimeoutHeader: []string{"invalid"}},
-			time.Second,
-			tuple{now.Add(time.Second), false},
-			10 * time.Millisecond,
-		},
-	}
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			deadline, present := test.header.Deadline(test.fallback)
-			assert.Equal(t, test.expected.present, present)
-			assert.WithinDuration(t, test.expected.deadline, deadline, test.precision)
-		})
-	}
-}
 
 func TestHeader_NoCache(t *testing.T) {
 	tests := map[string]struct {
@@ -121,44 +66,6 @@ func TestHeader_Strict(t *testing.T) {
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			assert.Equal(t, test.expected, test.header.Strict())
-		})
-	}
-}
-
-func TestHeader_Timeout(t *testing.T) {
-	type tuple struct {
-		duration time.Duration
-		present  bool
-	}
-
-	tests := map[string]struct {
-		header   Header
-		fallback time.Duration
-		expected tuple
-	}{
-		"exists in header": {
-			Header{XTimeoutHeader: []string{"100ms"}},
-			time.Second,
-			tuple{100 * time.Millisecond, true},
-		},
-		"fallback cause empty": {
-			nil,
-			time.Second,
-			tuple{time.Second, false},
-		},
-		"fallback cause invalid": {
-			Header{XTimeoutHeader: []string{"invalid"}},
-			time.Second,
-			tuple{time.Second, false},
-		},
-	}
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			duration, present := test.header.Timeout(test.fallback)
-			assert.Equal(t,
-				test.expected,
-				tuple{duration, present},
-			)
 		})
 	}
 }
