68e3a358c791b117453ca139ca4197a29a9f871e
Kamil Samigullin <kamil@samigullin.info>
2020-03-11 08:32:53 +0300

    add middleware stack

--- http/middleware/middleware.go
+++ http/middleware/middleware.go
@@ -4,3 +4,12 @@ import "net/http"
 
 // Middleware defines the Handler wrapper.
 type Middleware func(http.Handler) http.Handler
+
+type Stack []Middleware
+
+func (stack Stack) Apply(handler http.Handler) http.Handler {
+	for i := len(stack) - 1; i >= 0; i-- {
+		handler = stack[i](handler)
+	}
+	return handler
+}
