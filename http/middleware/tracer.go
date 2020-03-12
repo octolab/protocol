d4dfa41f56e36cac4e84aa80c238da6bd5870848
Kamil Samigullin <kamil@samigullin.info>
2020-03-12 08:05:36 +0300

    fix #1: finalize tracer middleware

--- http/middleware/tracer.go
+++ http/middleware/tracer.go
@@ -8,16 +8,15 @@ import (
 
 // Tracer returns the Middleware to inject a simple tracer
 // into the request context.
-func Tracer(buffer int) Middleware {
+func Tracer(buffer int, logger func(*tracer.Trace)) func(http.Handler) http.Handler {
 	return func(handler http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			handler.ServeHTTP(rw, req.WithContext(
-				tracer.Inject(req.Context(), make([]*tracer.Call, 0, buffer)),
-			))
+			ctx := tracer.Inject(req.Context(), make([]*tracer.Call, 0, buffer))
+			handler.ServeHTTP(rw, req.WithContext(ctx))
 
 			select {
 			case <-req.Context().Done():
-				// TODO:log
+				logger(tracer.Fetch(ctx))
 			default:
 				return
 			}
--- http/middleware/tracer_test.go
+++ http/middleware/tracer_test.go
@@ -1,1 +1,37 @@
 package middleware_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/kamilsk/tracer"
+	"github.com/stretchr/testify/assert"
+
+	. "go.octolab.org/toolkit/protocol/http/middleware"
+)
+
+func TestTracer(t *testing.T) {
+	var spy bool
+	middleware := Tracer(1, func(tracer *tracer.Trace) {
+		assert.Contains(t, tracer.String(), "call middleware_test.TestTracer.func2")
+		spy = true
+	})
+
+	var handler http.Handler
+	handler = http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		defer tracer.Fetch(req.Context()).Start().Stop()
+		time.Sleep(time.Millisecond)
+	})
+	handler = middleware(handler)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	handler.ServeHTTP(httptest.NewRecorder(), (&http.Request{}).WithContext(ctx))
+	assert.False(t, spy)
+
+	cancel()
+	handler.ServeHTTP(httptest.NewRecorder(), (&http.Request{}).WithContext(ctx))
+	assert.True(t, spy)
+}
