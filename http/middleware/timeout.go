da7a846d99e955232e53311e1fc643fe489c5c9b
Kamil Samigullin <kamil@samigullin.info>
2020-03-12 08:07:11 +0300

    change timer middlewares signature

--- http/middleware/timeout.go
+++ http/middleware/timeout.go
@@ -10,7 +10,10 @@ import (
 
 // Deadline returns the Middleware to inject a deadline timestamp
 // into the request context.
-func Deadline(fallback time.Duration, corrector func(time.Time, bool) time.Time) Middleware {
+func Deadline(
+	fallback time.Duration,
+	corrector func(time.Time, bool) time.Time,
+) func(http.Handler) http.Handler {
 	return func(handler http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			deadline := corrector(header.Deadline(req.Header, fallback))
@@ -23,7 +26,10 @@ func Deadline(fallback time.Duration, corrector func(time.Time, bool) time.Time)
 
 // Timeout returns the Middleware to inject a timeout duration
 // into the request context.
-func Timeout(fallback time.Duration, corrector func(time.Duration, bool) time.Duration) Middleware {
+func Timeout(
+	fallback time.Duration,
+	corrector func(time.Duration, bool) time.Duration,
+) func(http.Handler) http.Handler {
 	return func(handler http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			timeout := corrector(header.Timeout(req.Header, fallback))
