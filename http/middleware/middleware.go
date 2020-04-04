7aa5354b6fc80a1cdcaf1765b2cb0dab551703be
Kamil Samigullin <kamil@samigullin.info>
2020-04-04 17:59:11 +0300

    declare handlers for router and rest

--- http/middleware/middleware.go
+++ http/middleware/middleware.go
@@ -8,7 +8,7 @@ type Middleware func(http.Handler) http.Handler
 // Stack provides the method to wrap a Handler.
 type Stack []func(http.Handler) http.Handler
 
-// Apply wraps the Handler.
+// Apply wraps the Handler by the Stack of another handlers.
 func (stack Stack) Apply(handler http.Handler) http.Handler {
 	for i := len(stack) - 1; i >= 0; i-- {
 		handler = stack[i](handler)
